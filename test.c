e9cda3fdde9448b00f91800e654c0af1b78e2f96
jam <sksms1802@naver.com>
2023-10-01 21:49:26 +0900

    1_day

--- test.c
+++ test.c
@@ -1,17 +1,28 @@
 #include<stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
 
+struct termios orig_termios;
+
+void diableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode(){
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO);
+    
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}   
 
 int main(){
-    printf("Hello World\n");
-    printf("Hello World\n");
-    printf("Hello World\n");
-    printf("Hello World\n");
-    printf("Hello World\n");
-    printf("Hello World\n");
-    printf("Hello World\n");
-    printf("Hello World\n");
-    printf("Hello World\n");
-    
+    enableRawMode();
 
+    char c;
+    While (read(STDIN_FILENO, &c, 1) == 1 && c!= 'q');
     return 0;
 }

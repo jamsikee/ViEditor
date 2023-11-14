8da948889b9a089b7a54d6cf270838b521c3edd6
jamsikee <sksms1802@naver.com>
2023-11-14 15:14:51 +0900

    No.1

--- origin.c
+++ origin.c
@@ -4,3 +4,24 @@
 #include <termios.h>
 #include <unistd.h>
 
+void raw() {
+  struct termios raw = E.orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+}
+
+int main(){
+    raw();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
+}

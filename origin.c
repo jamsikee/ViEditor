cc5520e8f44a07562c5cfd597b07f6ac91e82743
jamsikee <sksms1802@naver.com>
2023-11-15 17:00:36 +0900

    presskey

--- origin.c
+++ origin.c
@@ -23,16 +23,19 @@ enum P_key{
 
 void Raw() {
     struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 void presskey(){
     char c;
+    read(STDIN_FILENO, &c, 1)
     switch (c)
     {
     case CONTROL('q'):

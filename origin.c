827839583501af96b010ccca016b00c70c7bffd2
jamsikee <sksms1802@naver.com>
2023-11-15 16:45:29 +0900

    presskey

--- origin.c
+++ origin.c
@@ -6,6 +6,20 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <ctype.h>
+#define CONTROL(k) ((k) & 0x1f)  // 문자의 아스키코드 지정 ctrl-A = 1
+
+enum P_key{
+    B_space,
+    left,
+    right,
+    up,
+    down,
+    Del,
+    End,
+    Home,
+    PgUp,
+    PgDn
+};
 
 void Raw() {
     struct termios raw;
@@ -17,14 +31,50 @@ void Raw() {
     raw.c_cc[VTIME] = 1;
 }
 
+void presskey(){
+    char c;
+    switch (c)
+    {
+    case CONTROL('q'):
+        exit(0); // 프로그램 종료
+        break;
+    case CONTROL('s'):
+        break;
+    case CONTROL('f'):
+        break;    
+    case Home:
+        break;
+    case B_space:
+        break;
+    case Del:
+        break;
+    case left:
+        break;
+    case right:
+        break;
+    case up:
+        break;
+    case down:
+        break;
+    case End:
+        break;
+    case Home:
+        break;
+    case PgUp:
+        break;
+    case PgDn:
+        break;
+
+    default:
+        break;
+    }
+}
+
 int main(){
     Raw();
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-    if (iscntrl(c)) {
-      printf("%d\n", c);
-    } else {
-      printf("%d ('%c')\n", c, c);
-    }
+    while (1) {
+    presskey();
   }
+  return 0;
 }

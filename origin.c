5262cc95661f533200dcc6bf2b5b95b092cba670
jamsikee <sksms1802@naver.com>
2023-11-21 16:18:41 +0900

    323232

--- origin.c
+++ origin.c
@@ -1,276 +1,130 @@
-#include <stdio.h>
+#include <ncurses.h>
 #include <stdlib.h>
 #include <string.h>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <ctype.h>
-#include <ncurses.h>
-#include <signal.h>
 
-#define CONTROL(k) ((k) & 0x1f)
+/*** data ***/
 
-struct termios orig_termios;
-
-enum P_key {
-    B_space = 127,
-    left = 1000,
-    right,
-    up,
-    down,
-    Del,
-    End,
-    Home,
-    PgUp,
-    PgDn
-};
-
-struct Cursor {
-    int x, y;
-    int rows;
-    int cols;
-    int currentrows;
-};
-
-struct Cursor C;
-
-struct LineRow {
-    char *chars;
+typedef struct erow {
     int size;
-    struct LineRow *next;
-};
+    char *chars;
+} erow;
 
-void disRaw() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+/*** Global Variables ***/
+int cx = 0, cy = 0;
+int screenrows, screencols;
+erow row;
+
+/*** File I/O ***/
+
+void editorOpen() {
+    char *line = "Hello, world!";
+    ssize_t linelen = 13;
+
+    row.size = linelen;
+    row.chars = malloc(linelen + 1);
+    memcpy(row.chars, line, linelen);
+    row.chars[linelen] = '\0';
 }
 
-void Raw() {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    tcgetattr(STDIN_FILENO, &raw);
+/*** Output ***/
 
-    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    raw.c_oflag &= ~(OPOST);
-    raw.c_cflag |= (CS8);
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    raw.c_cc[VMIN] = 0;
-    raw.c_cc[VTIME] = 1;
+void editorDrawRows() {
+    for (int y = 0; y < screenrows; y++) {
+        if (y >= 1) {
+            if (y == screenrows / 3) {
+                char welcome[80];
+                int welcomelen = snprintf(welcome, sizeof(welcome),
+                                          "Kilo editor -- version 0.0.1");
+                if (welcomelen > screencols) welcomelen = screencols;
+                int padding = (screencols - welcomelen) / 2;
+                if (padding) {
+                    mvaddch(y, 0, '~');
+                    padding--;
+                }
+                while (padding--) mvaddch(y, 0, ' ');
+                mvprintw(y, padding, welcome);
+            } else {
+                mvaddch(y, 0, '~');
+            }
+        } else {
+            int len = row.size;
+            if (len > screencols) len = screencols;
+            mvprintw(y, 0, "%s", row.chars);
+        }
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-    atexit(disRaw);
-}
-
-void Append(struct LineRow **row, const char *s, int len) {
-    struct LineRow *newRow = malloc(sizeof(struct LineRow));
-    if (newRow == NULL) return;
-
-    newRow->chars = malloc(len + 1);
-    if (newRow->chars == NULL) {
-        free(newRow);
-        return;
-    }
-
-    memcpy(newRow->chars, s, len);
-    newRow->chars[len] = '\0';
-    newRow->size = len;
-    newRow->next = NULL;
-
-    if (*row == NULL) {
-        *row = newRow;
-        return;
-    }
-
-    struct LineRow *current = *row;
-    while (current->next != NULL) {
-        current = current->next;
-    }
-    current->next = newRow;
-}
-
-struct LineRow *Insert(struct LineRow *row, const char *s, int len) {
-    struct LineRow *newRow = malloc(sizeof(struct LineRow));
-    newRow->chars = malloc(len + 1);
-    memcpy(newRow->chars, s, len);
-    newRow->chars[len] = '\0';
-    newRow->size = len;
-    newRow->next = row;
-    return newRow;
-}
-
-void freeRow(struct LineRow *row) {
-    while (row) {
-        struct LineRow *temp = row->next;
-        free(row->chars);
-        free(row);
-        row = temp;
+        clrtoeol();
     }
 }
 
-void editorDrawRows(struct LineRow *row) {
-    int y;
+void editorRefreshScreen() {
     clear();
-    for (y = 0; y < C.rows; y++) {
-        mvprintw(y, 0, "~");
-    }
-
-    if (C.rows / 3 >= 0 && C.rows / 3 < C.rows && C.currentrows == 0) {
-        char welcome[80];
-        int welcomelen = snprintf(welcome, sizeof(welcome), "Visual Text editor -- version 0.0.1");
-        if (welcomelen > C.cols) welcomelen = C.cols;
-        int padding = (C.cols - welcomelen) / 2;
-        mvprintw(C.rows / 3, padding > 0 ? padding : 0, "%s", welcome);
-    }
-
-    struct LineRow *current = row;
-    int row_count = 0;
-    while (current != NULL && row_count < C.rows) {
-        mvprintw(row_count, 0, current->chars);
-        current = current->next;
-        row_count++;
-    }
-}
-
-void Refresh(struct LineRow *row){
-    editorDrawRows(row);
-    move(C.y, C.x);
+    editorDrawRows();
+    move(cy, cx);
     refresh();
 }
 
-void Move(int key) {
-    switch (key) {
-        case left:
-            if (C.x != 0) {
-                C.x--;
-            }
-            break;
-        case right:
-            if (C.x != C.cols - 1) {
-                C.x++;
-            }
-            break;
-        case up:
-            if (C.y != 0) {
-                C.y--;
-            }
-            break;
-        case down:
-            if (C.y != C.rows - 1) {
-                C.y++;
-            }
-            break;
-    }
-}
+/*** Input ***/
 
-void presskey(struct LineRow **row) {
+void editorProcessKeypress() {
     int c = getch();
 
     switch (c) {
-        case CONTROL('q'):
-            freeRow(*row);
+        case 'q':
+        case 'Q':
             endwin();
             exit(0);
             break;
-        case KEY_LEFT: // 왼쪽 화살표 키
-        case KEY_RIGHT: // 오른쪽 화살표 키
-        case KEY_UP: // 위쪽 화살표 키
-        case KEY_DOWN: // 아래쪽 화살표 키
-            Move(c);
-            break;
-        case KEY_END: // End 키
-            C.x = C.cols - 1;
-            break;
-        case KEY_HOME: // Home 키
-            C.x = 0;
-            break;
-        case '\n': // Enter 키
-            *row = Insert(*row, "", 0);
-            Move(down);
-            C.x = 0;
-            break;
-        case KEY_NPAGE: // Page Down 키
-        case KEY_PPAGE: // Page Up 키
-        {
-            int temprows = C.rows;
-            while (temprows--) {
-                if (c == KEY_PPAGE)
-                    Move(up);
-                else if (c == KEY_NPAGE)
-                    Move(down);
+
+        case KEY_LEFT:
+            if (cx != 0) {
+                cx--;
             }
-        }
+            break;
+
+        case KEY_RIGHT:
+            if (cx != screencols - 1) {
+                cx++;
+            }
+            break;
+
+        case KEY_UP:
+            if (cy != 0) {
+                cy--;
+            }
+            break;
+
+        case KEY_DOWN:
+            if (cy != screenrows - 1) {
+                cy++;
+            }
             break;
     }
 }
 
-void editorOpen(char *filename, struct LineRow **row) {
-    FILE *fp = fopen(filename, "r");
-    if (!fp) {
-        perror("fopen");
+/*** Initialization ***/
+
+void initEditor() {
+    if (getmaxyx(stdscr, screenrows, screencols) == ERR) {
+        endwin();
+        fprintf(stderr, "Failed to get screen size!\n");
         exit(1);
     }
-
-    char *line = NULL;
-    size_t linecap = 0;
-    ssize_t linelen;
-
-    while ((linelen = getline(&line, &linecap, fp)) != -1) {
-        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
-            linelen--;
-        }
-
-        struct LineRow *newRow = malloc(sizeof(struct LineRow));
-        if (!newRow) {
-            fclose(fp);
-            free(line);
-            exit(1);
-        }
-
-        newRow->chars = malloc(linelen + 1);
-        if (!newRow->chars) {
-            fclose(fp);
-            free(line);
-            free(newRow);
-            exit(1);
-        }
-
-        memcpy(newRow->chars, line, linelen);
-        newRow->chars[linelen] = '\0';
-        newRow->size = linelen;
-        newRow->next = NULL;
-
-        Append(row, newRow->chars, newRow->size);
-
-        C.currentrows++;
-    }
-
-    free(line);
-    fclose(fp);
+    editorOpen();
 }
 
-void init() {
-    Raw();
+int main() {
     initscr();
-    getmaxyx(stdscr, C.rows, C.cols);
+    raw();
     keypad(stdscr, TRUE);
-    C.x = 0;
-    C.y = 0;
-    C.currentrows = 0;
+    noecho();
 
-}
-
-int main(int argc, char *argv[]) {
-
-    struct LineRow *row = NULL;
-    init();
-    editorDrawRows(row);
-    if (argc >= 2) {
-    editorOpen(argv[1], &row);
-    }
+    initEditor();
 
     while (1) {
-        presskey(&row);
-        Refresh(row);
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
+
+    endwin();
     return 0;
 }

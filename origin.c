95cf7ab9f66ce76b592e9df1116cc5fb9cf55b6f
jamsikee <sksms1802@naver.com>
2023-12-05 14:51:26 +0900

    file test1

--- origin.c
+++ origin.c
@@ -516,11 +516,11 @@ int main(int argc, char *argv[]) {
   if (argc >= 2) {
     open_file(argv[1]);
   }
-    printf("Total lines: %d\n", Edit.total);
-  for (int i = 0; i < Edit.total; i++) {
-    printf("Line %d: %s\n", i, Edit.line[i].c);
-  }
 
+  for (int i = 0; i < Edit.total; i++) {
+    printf("%s\n", i, Edit.line[i].c);
+  }
+  printf("Total lines: %d\n", Edit.total);
   while (1) {
     presskey();
     

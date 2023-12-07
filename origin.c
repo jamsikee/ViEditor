2ca4c18b8632912b4f2330188554893932f3f8f6
jamsikee <sksms1802@naver.com>
2023-12-08 07:09:00 +0900

    sdsds

--- origin.c
+++ origin.c
@@ -284,7 +284,7 @@ void status_bar() {
     mvprintw(rows - 2, 0, "[%s] - %d lines", Edit.filename, Edit.total);
     int left_len = strlen(total) + strlen(Edit.filename) + 13;
     int right_len = strlen(total) + strlen(st_y) + 9; // 9은 "no ft | "의 길이
-    int total_len = cols - left_len - right_len;
+    int total_len = cols - right_len;
     for (int i = left_len; i < total_len; i++){
       mvprintw(rows-2, i, " ");
     }

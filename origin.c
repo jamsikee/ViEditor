0c0a6e13c247758da829d226a7185966e8a23a48
jamsikee <sksms1802@naver.com>
2023-12-08 07:14:18 +0900

    sdsds

--- origin.c
+++ origin.c
@@ -286,7 +286,7 @@ void status_bar() {
     int right_len = strlen(total) + strlen(st_y) + 9; // 9은 "no ft | "의 길이
 
     for (int i = left_len; i < cols -right_len; i++){
-      mvprintw(rows - 2, i, " ");
+      mvprintw(" ");
     }
     // 오른쪽에 텍스트 출력
     mvprintw(rows - 2, cols - right_len, "no ft | %d/%d", y, Edit.total);

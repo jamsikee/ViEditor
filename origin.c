78516c8a7745a1d1f0918b98a701d62a24c5a95b
jamsikee <sksms1802@naver.com>
2023-12-08 18:26:31 +0900

    dsds

--- origin.c
+++ origin.c
@@ -362,7 +362,11 @@ void Move(int key) {
 
 
 void presskey() {
-   
+
+     for (int i = 0; i < total; i++){
+      mvprintw(i, x, "%*s", cols - x, " ");
+    }
+
     int c = getch();
 
     switch (c) {

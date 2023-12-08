e4a5c8ba07df6e75a7d9e376e32ad9bf8b0cb4b8
jamsikee <sksms1802@naver.com>
2023-12-08 15:48:09 +0900

    sdsdssd

--- origin.c
+++ origin.c
@@ -49,9 +49,8 @@ void Insertchar(char word);
 void Del_current_line_char();
 void Del_current_line();
 void DeleteChar();_
-void line(Row *line, int pos_y, int pos_x);
+void contained_new_line(Row *line, int pos_y, int pos_x);
 void Newline();
-void contained_new();
 void status_bar();
 void state();
 void end_message( const char *format, ...);

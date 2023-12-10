63880b710662770c9d6b94efde543bcea13efa14
jamsikee <sksms1802@naver.com>
2023-12-10 16:25:43 +0900

    tt

--- origin.c
+++ origin.c
@@ -303,7 +303,7 @@ void status_bar() {
 
     // 오른쪽에 텍스트 출력
 
-    mvprintw(rows - 2, cols - right_len, "no ft | %d/%d", y + y_out + 1, total/10);
+    mvprintw(rows - 2, cols - right_len, "no ft | %d/%d", y + y_out + 1, total);
 
     attroff(COLOR_PAIR(2) | A_REVERSE); // Turn off the reverse color pair
 }

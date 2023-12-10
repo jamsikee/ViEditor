7a4b2a2c8fa06e5b4c702209c847d64c7e44997a
jamsikee <sksms1802@naver.com>
2023-12-10 19:07:54 +0900

    tt

--- origin.c
+++ origin.c
@@ -326,7 +326,7 @@ void Move(int key) {
             }
             break;
         case KEY_RIGHT:
-          if(Edit.line[y] == NULL){
+          if(line[y] == NULL){
             break;
           } else{
             if ( x < Edit.line[y].len){

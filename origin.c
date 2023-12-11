c912f2ae149821628005c6d7cc2b22ab48eb7106
jamsikee <sksms1802@naver.com>
2023-12-11 09:15:08 +0900

    tt

--- origin.c
+++ origin.c
@@ -541,6 +541,7 @@ void scroll_clean_and_printing(int pos)
   for (int i = pos; i < rows - 2; ++i)
   {
     mvprintw(i, 0, "%*s", cols, "");
+    mvprintw(i, 0, "%*s", cols, "~");
   }
 
   for (int i = 0; i <= y; ++i){

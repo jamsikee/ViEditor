a2b6b56b6dd8ec854574f936cbbeeb57ee1ca58b
jamsikee <sksms1802@naver.com>
2023-12-11 14:48:26 +0900

    tt

--- origin.c
+++ origin.c
@@ -419,8 +419,8 @@ void Move(int key)
     else if (x == 0 && y == 0){
       if(y_out > 0){
         y = 0;
-        x = Edit.line[y + y_out].len;
         y_out -= 1;
+        x = Edit.line[y + y_out].len;
       }
     }
     move(y, x);

84bf3e1ffc8825b3cf5b53cfd484a55ea292ee31
jamsikee <sksms1802@naver.com>
2023-12-11 17:01:31 +0900

    tt

--- origin.c
+++ origin.c
@@ -650,11 +650,11 @@ void delete_clean_and_printing(int pos)
 
   for (int i = 0; i < rows - 2; ++i)
   {
-    if (Edit.line[i + y_out].c == NULL)
+    if (Edit.line[i + y_out + 1].c == NULL)
     {
       mvprintw(i, 0, "%*s", cols, "");
       mvprintw(i, 0, "~");
-      break;
+      continue;
     }
     else
     {

613c533ba413c35824106ddb7e9b4981703f9920
jamsikee <sksms1802@naver.com>
2023-12-13 18:31:36 +0900

    tt

--- origin.c
+++ origin.c
@@ -716,10 +716,9 @@ void sub_matching(char *Query)
   int s_size = 0;
   s_pos->s_total = 0;
   s_size = Search_SIZE;
-  s_pos->s_out = 0;
-  s_pos->s_x = 0;
-  s_pos->s_y = 0;
-  free(s_pos);
+  if (s_pos != NULL) {
+    free(s_pos);
+}
   s_pos = malloc(sizeof(SearchPosition) * s_size); // first malloc 10000
   for (int i = 0; i < total; ++i)
   {

d144dcae25d8127d504f4792afd4523f3c12d3f9
jamsikee <sksms1802@naver.com>
2023-12-13 19:09:58 +0900

    find test

--- origin.c
+++ origin.c
@@ -843,7 +843,7 @@ void presskey()
           else if(s_c == KEY_RIGHT){
             if(s_now < s_pos->s_total - 1){
               s_now += 1;
-            }else if(s_now == s_pos->s_total){
+            }else if(s_now > s_pos->s_total){
               s_now = 0;
             }
           }
